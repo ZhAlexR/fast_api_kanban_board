1d4fdc8b22f282108db65f8ed1582a914b05ba55
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-09-19 22:01:25 +0300

    Refactored code-style

--- beckend/app/main.py
+++ beckend/app/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from beckend.app import models
+from beckend.app.database import engine
+
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000"
-]
+origins = ["http://localhost:3000"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins
-)
+app.add_middleware(CORSMiddleware, allow_origins=origins)
+
+models.Base.metadata.create_all(bind=engine)
