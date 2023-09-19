0369d25b28973175a3efb8ab45be80f9b1c1f8a4
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-09-19 22:01:53 +0300

    Added new models Role and Permission

---
+++ beckend/app/models.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column, String, Integer, Table, ForeignKey
+from sqlalchemy.orm import relationship
+
+from beckend.app.database import Base
+
+role_permission_association = Table(
+    "role_permission",
+    Base.metadata,
+    Column("role_id", Integer, ForeignKey("roles.id")),
+    Column("permission_id", Integer, ForeignKey("permissions.id")),
+)
+
+
+class Role(Base):
+    __tablename__ = "roles"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    permissions = relationship(
+        "Permission", secondary=role_permission_association, back_populates="roles"
+    )
+
+
+class Permission(Base):
+    __tablename__ = "permissions"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    roles = relationship(
+        "Role", secondary=role_permission_association, back_populates="permissions"
+    )
