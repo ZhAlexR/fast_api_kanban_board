9cbd3a9c5132a84c6fb30057c01894741cec2915
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-10-04 01:42:14 +0300

    Added User model

--- backend/app/models.py
+++ backend/app/models.py
@@ -17,10 +17,12 @@ class Role(Base):
     __tablename__ = "roles"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String, unique=True)
     permissions = relationship(
         "Permission", secondary=role_permission_association, back_populates="roles"
     )
+    user_id = Column(Integer, ForeignKey("users.id"), default=None)
+    user = relationship("User", back_populates="role")
 
 
 class Permission(Base):
@@ -31,3 +33,14 @@ class Permission(Base):
     roles = relationship(
         "Role", secondary=role_permission_association, back_populates="permissions"
     )
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    surname = Column(String)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    role = relationship("Role", uselist=False, back_populates="user")
