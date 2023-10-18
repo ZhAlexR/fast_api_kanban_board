70a87b4c917aafb68a18ce841c385bb8d0dffb75
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-10-19 02:00:18 +0300

    Moved config.py to the separate folder

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-from backend.app.config import settings
+from backend.configuration.config import settings
 
 engine = create_engine(settings.DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
