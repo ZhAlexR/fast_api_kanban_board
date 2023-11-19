463303d72d3ad41a818956bb735b33a3c4e63ff4
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-11-19 03:14:40 +0200

    Added current user extraction

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,4 +1,15 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+
 from backend.app.database import SessionLocal
+from backend.app.models import User
+from backend.app.repository.user import user
+from backend.app.services.security import decode_access_token
+
+reusable_oauth2 = OAuth2PasswordBearer(
+    tokenUrl="/login/get-token"
+)
 
 
 def get_db():
@@ -7,3 +18,15 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def get_current_user(
+        db: Session = Depends(get_db),
+        token: str = Depends(reusable_oauth2)
+) -> User:
+    token_data = decode_access_token(access_token=token)
+    current_user = user.get_by_email(db, email=token_data)
+    if not current_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return current_user
+
